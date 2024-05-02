c1f5551f87d577de90f4faffbeb5f526b571d9d7
yejinhaha <yeajin782@gmail.com>
2024-05-02 14:43:56 +0900

    new commit

--- ARmap/tutorial_multi_language_app/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.tutorialmultilanguageapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ARmap/tutorial_multi_language_app/lib/index.dart
+++
@@ -1,8 +0,0 @@
-// // Export pages
-// export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-// export '/pages/profile04/profile04_widget.dart' show Profile04Widget;
-// export '/pages/auth_home/auth_home_widget.dart' show AuthHomeWidget;
-// export '/pages/auth_welcome_screen/auth_welcome_screen_widget.dart'
-//     show AuthWelcomeScreenWidget;
-// export '/pages/auth_forgot_password/auth_forgot_password_widget.dart'
-//     show AuthForgotPasswordWidget;
